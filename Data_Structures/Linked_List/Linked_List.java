5ab7f640d75e8716476584c040889992b0ed1554
ASUS <ASUS@DESKTOP-K1G6C3D>
2022-12-15 00:23:49 +0530

    Added append & remove last

--- Data_Structures/Linked_List/Linked_List.java
+++ Data_Structures/Linked_List/Linked_List.java
@@ -18,25 +18,65 @@ public class Linked_List {
 		Node temp = head;
 		while(temp != null) {
 			
-			System.out.println(temp.value);
+			System.out.print(" "+temp.value);
 			temp= temp.nest;
 		}
 	}
 	
 	public void getHead() {
 		
-		System.out.println("Head : "+head.value);
+		System.out.println(" Head : "+ head.value);
 	}
 	
 	public void getTail() {
 		
-		System.out.println("Tail : "+tail.value);
+		System.out.println(" Tail : "+tail.value);
 	}
 	
 	public void getLenght() {
 		
-		System.out.println("Lenght : "+lenght);
+		System.out.println(" Lenght : "+lenght);
 	}
-
+	
+	public void append(int value){
+		
+		Node newnode = new  Node(value);
+		if(lenght == 0) {
+			head = newnode;
+			tail = newnode;
+		}
+		else {
+			tail.nest = newnode;
+			tail = newnode;
+		}
+		lenght++;
+		
+	}
+	
+	public void removeLast() {
+		
+		if(lenght == 0) {
+			System.out.print("Null");
+		}
+		
+		Node temp = head;
+		Node pre = head;
+		while(temp.nest != null) {
+			
+			pre = temp;
+			temp = temp.nest;
+		}
+		
+		tail = pre;
+		tail.nest = null;
+		lenght--;
+		
+		if(lenght ==0) {
+			head = null;
+			tail = null;
+		}
+		
+		System.out.println("\n Remove: "+temp.value);
+	}
+	
 }
-
--- Data_Structures/Linked_List/main.java
+++ Data_Structures/Linked_List/main.java
@@ -4,13 +4,22 @@ public class main {
 
 	public static void main(String[] args) {
 		
-		Linked_List ll = new Linked_List(7);
+		Linked_List ll = new Linked_List(11);
 		
-		ll.pirntList();
+		ll.append(3);
+		ll.append(23);
+		ll.append(7);
 		
 		ll.getHead();
 		ll.getTail();
 		ll.getLenght();
+		
+		ll.pirntList();
+		
+		ll.removeLast();
+		
+		
+		ll.pirntList();
 	}
 
 }
