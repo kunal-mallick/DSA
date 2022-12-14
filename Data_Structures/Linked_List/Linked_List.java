5be917dc77f5630e473d9597458229473fb5158d
ASUS <ASUS@DESKTOP-K1G6C3D>
2022-12-14 20:04:19 +0530

    created linked-list main class & print funtion

--- Data_Structures/Linked_List/Linked_List.java
+++ Data_Structures/Linked_List/Linked_List.java
@@ -1,5 +1,27 @@
 package Linked_List;
 
 public class Linked_List {
+	
+	Node head;
+	Node tail;
+	int lenght;
+	
+	public Linked_List(int value) {
+		Node newnode = new  Node(value);
+		head = newnode;
+		tail = newnode;
+		lenght = 1;
+	}
+	
+	public void pirntList() {
+		
+		Node temp = head;
+		while(temp != null) {
+			
+			System.out.println(temp.value);
+			temp= temp.nest;
+		}
+	}
 
 }
+
---
+++ Data_Structures/Linked_List/main.java
@@ -0,0 +1,13 @@
+package Linked_List;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		Linked_List ll = new Linked_List(7);
+		
+		ll.pirntList();
+
+	}
+
+}
